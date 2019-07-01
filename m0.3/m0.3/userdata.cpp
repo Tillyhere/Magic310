d964b76d51fc8e62dce4d1f118a9b61a847544c7
Tillyhere <2077548638@qq.com>
2019-07-02 03:19:53 +0800

    tenth submission

--- m0.3/m0.3/userdata.cpp
+++ m0.3/m0.3/userdata.cpp
@@ -11,26 +11,26 @@
 
 void userdata::search()
 {
-	cout<<"~查询用户"<<endl;
+	cout<<"~Query User Information"<<endl;
 	fstream f(filename,ios::in|ios::binary);
 	do {
-		cout<<"1.ID查询 2.用户名查询 else.返回主菜单 :";
+		cout<<"1.ID Query 2.Username Query else.return menu :";
 		int targetid=0;
 		cin>>targetid;
 		if(targetid==1) {
-			cout<<"\t请输入用户ID :";
+			cout<<"\t Plesae input user's ID :";
 			cin>>targetid;
 			f.seekg((int)(targetid*(2+sizeof(userdata::temp_data)+1)));
 			readData(f);
-			cout<<"\nID\t用户名\t\t黑名单状态"<<endl;
+			cout<<"\nID\tUsername\t\tBlacklist"<<endl;
 			showData();
 		} else if(targetid==2) {
 			string sample;
 			string matchsample;
 			f.seekg(0);
-			cout<<"\t请输入用户名关键字 :";
+			cout<<"\tPlease input username's keywords";
 			cin>>matchsample;
-			cout<<"\nID\t用户名\t\t黑名单状态"<<endl;
+			cout<<"\nID\tUsername\t\tBlacklist"<<endl;
 			readData(f);
 			do { //开始遍历文件
 				sample=t.name;
@@ -41,19 +41,20 @@ void userdata::search()
 			} while(!f.eof());
 			f.clear();
 		}
-		cout<<"_________________________搜索结束"<<endl;
-		cout<<"\n1.继续查询 else.返回主菜单 :";
+		cout<<"_________________________Query End"<<endl;
 		cin.ignore(1024,'\n');
+		cout<<"\n1.Continue else.Return menu :";
 	} while(cin.get()=='1');
 	f.close();
 }
 
 void userdata::createProfile()
 {
-	cout<<"~创建会员卡"<<endl;
-	cout<<"提示：1.第一次创建时，id段起始数请输入0，用户id从0开始\n"
-	    <<"      2.创建新id段会覆盖原id段信息（姓名、密码及租借记录）。可用于覆盖原用户信息。"<<endl;
-	cout<<"1.开始创建 else.返回主菜单 :";
+	cout<<"~Membership Card Transaction"<<endl;
+	cout<<"Tips：1.When it is first created，Please input 0 into the starting number of ID segment\n"
+	
+	    <<"      2.Creat new ID segment will cover the old information（name,passwords and rental record)."<<endl;
+	cout<<"1.Get started else.Return menu:";
 	cin.ignore(1024,'\n');
 	if(cin.get()=='1') {
 		fstream f(filename,ios::in|ios::out|ios::binary);
@@ -61,10 +62,10 @@ void userdata::createProfile()
 		firstlogin=true;
 		int startid=0,endid=0;
 		do {
-			cout<<"\t请输入id段的起始数 终止数 :" ;
+			cout<<"\tPlease input the starting number and ending number of ID segment" ;
 			cin>>startid>>endid;
 			if(cin.fail()) { //避免错误输入污染文件
-				cout<<"_________________________警告：输入了错误信息！"<<endl;
+				cout<<"_________________________Warning:Wrong Information was enterd"<<endl;
 				cin.clear();
 				cin.ignore(1024,'\n');
 				continue; //重新输入
@@ -75,11 +76,11 @@ void userdata::createProfile()
 					writeData(f);
 					sprintf(logfilename,"./datas/userdatas/%d.csv",t.id);
 					fstream f_log(logfilename,ios::out); //创建租借记录文件
-					f_log<<"时间"<<','<<"用户名"<<','<<"用户ID"<<','<<"租出/归还"<<','<<"影片名"<<','<<"影片ID"<<endl;
+					f_log<<"Datetime"<<','<<"Username"<<','<<"User'ID"<<','<<"Rent/Return"<<','<<"Movie's name'"<<','<<"Movie's ID"<<endl;
 					f_log.close();
 				}
 
-				cout<<"_________________________创建id段"<<startid<<"至"<<endid<<"成功";
+				cout<<"_________________________Creat the segment of ID"<<startid<<"to"<<endid<<"successfully";
 				sleep(1);
 				break;
 			}
@@ -90,20 +91,20 @@ void userdata::createProfile()
 
 void userdata::manageID()
 {
-	cout<<"~用户ID管理"<<endl;
-	cout<<"提示：\t1.拉黑用户ID将拒绝该用户登录系统，但保留ID租借信息。"<<endl
-	    <<"\t2.解封用户ID将恢复被拉黑用户的权限，但无法恢复被回收的数据。"<<endl
-	    <<"\t3.回收用户ID将彻底删除该ID的用户信息和租借记录,ID重置为未注册状态。"<<endl;
-	cout<<"1.输入ID else.返回主菜单 :";
+	cout<<"~ID Management"<<endl;
+	cout<<"T：\t1.Block the user'ID:Access permission will be cancelled,while information of the ID will be remained."<<endl
+	    <<"\t2.Unblocek the ID:The authority will be recoverd,recycled data irreparable."<<endl
+	    <<"\t3.Recycle the ID:Delete the ID's information and rental record completely.Reset it back to its initial state"<<endl;
+	cout<<"1.Input ID else.return menu:";
 	cin.ignore(1024,'\n');
 	if(cin.get()=='1') {
 		fstream f(filename,ios::in|ios::out|ios::binary);
 		int targetid=0;
 		do {
-			cout<<"\t请输入目标用户ID :" ;
+			cout<<"\tPlease input target users'ID :" ;
 			cin>>targetid;
 			if(cin.fail()) { //避免错误输入污染文件
-				cout<<"_________________________警告：输入了错误信息！"<<endl;
+				cout<<"_________________________Warning:wrong information was entered"<<endl;
 				cin.clear();
 				cin.ignore(1024,'\n');
 				continue; //重新输入
@@ -113,14 +114,14 @@ void userdata::manageID()
 				f.seekp((int)(targetid*(2+sizeof(userdata::temp_data)+1)));
 				cout<<"\n用户ID:\t"<<t.id
 				    <<"\n用户名:\t"<<t.name<<endl;
-				cout<<"1.解封该ID 2.拉黑并回收ID 3.仅拉黑该ID else.取消操作 :";
+				cout<<"1.Unblock the ID 2.Block and recycle the ID 3.Block the ID only else.Cancel:";
 				cin.ignore(1024,'\n');
 				char logfilename[28]= {0};
 				switch(cin.get()) {
 					case '2':
 						sprintf(logfilename,"./datas/userdatas/%d.csv",targetid);
 						if(remove(logfilename)) { //若删除失败
-							cout<<"\n权限异常，请手动删除"<<logfilename<<"租借记录文件"<<endl;
+							cout<<"\nAbnormal permission!Please delete it mannually"<<logfilename<<"locker rental record"<<endl;
 						}
 						f.put(true); // firstlogin=true
 					case '3':
@@ -131,7 +132,7 @@ void userdata::manageID()
 						break;
 				}
 
-				cout<<"_________________________操作成功";
+				cout<<"_________________________Operation completed successfully";
 				sleep(1);
 				break;
 			}
@@ -166,13 +167,13 @@ void userdata::manageProfile(int targetid)  //客户端管理员端共有函数
 	readData(f);
 	cout<<"\n用户ID:\t"<<t.id
 	    <<"\n用户名:\t"<<t.name<<endl;
-	cout<<"1.确认修改此ID else.取消操作 :";
+	cout<<"1.Confirm the change of ID else.Cancel:";
 	cin.ignore(1024,'\n');
 	if(cin.get()=='1') {
 		f.seekp((int)(targetid*(2+sizeof(userdata::temp_data)+1)+1)); //这里不关心banned位
 		f.put(false); // firstlogin=true
-		cout<<"\n请输入信息(首字符输入\'%\'表示不做更改)"<<endl;
-		cout<<"用户名:";
+		cout<<"\nPlease input your information(If the first character is \'%\',no modification will be allowd)"<<endl;
+		cout<<"Username:";
 		cin.ignore(1024,'\n');
 		cin.get(t.name,30);
 		if(t.name[0]=='%') {
@@ -190,14 +191,14 @@ void userdata::manageProfile(int targetid)  //客户端管理员端共有函数
 				char pwdcheck[30]= {0};
 				inputPWD(pwdcheck,30);
 				if(strcmp(pwdcheck,t.pwd)) {
-					cout<<"\t输入密码不一致!"<<'\r';
+					cout<<"\tThe two passwords you typed do not match"<<'\r';
 					continue;
 				}
 				encryptPWD(t.pwd,targetid); //密码加密
 			}
 		} while(false);
 		f.write((char *)&t.pwd,30); //写入新密码
-		cout<<"\n_________________________操作成功";
+		cout<<"\n_________________________Operation completed successfully";
 	}
 	f.close();
 }
